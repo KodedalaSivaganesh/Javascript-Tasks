daf8d8dfa084ddd0b918b99bd9d92ec1dfa7e78d
KodedalaSivaganesh <kodedalasivaganeshbabu@thestackly.com>
2026-07-16 23:51:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,130 @@
+//1. Employee Registration 
+const Company = "Stackly";
+let Employee_Name = prompt("Enter the Employee Name");
+let Employee_ID = "EMP100";
+console.log("Company : "+Company);  // Company : Stackly
+console.log("Employee Name : "+Employee_Name); // Employee Name : Sivaganesh
+console.log("Employee ID : "+Employee_ID); // Employee ID : EMP100
+
+//2. College Admission
+const College_Name = "Vel Tech University";
+let Student_Name = prompt("Enter the name of the student");
+let Course_Name = prompt("Enter the name of the course");
+console.log("College Name : "+College_Name); // College Name : Vel Tech University
+console.log("Student Name : "+Student_Name); // Student Name : Sivaganesh
+console.log("Course Name : "+Course_Name);   // Course Name : ECE
+
+//3. Food Order Bill
+const Restaurant_Name = "SS Biryani";
+let Customer_Name = prompt("Enter the customer name");
+let Food_Name = prompt("Enter the name of the order");
+let Quantity = 1
+console.log("Restaurant Name : "+Restaurant_Name); // Restaurant Name : SS Biryani
+console.log("Customer Name : "+Customer_Name); // Customer Name : Sivaganesh
+console.log("Name of the item: "+Food_Name); // Name of the item : Chicken Biryani
+console.log("Quantity: "+Quantity); // Quantity : 1
+
+//4. Hospital Registration 
+const Hospital_Name = "Apollo Pvt Ltd";
+let Patient_Name = prompt("Name of the patient");
+let Token_Number = 23;
+console.log("Hospital Name : "+Hospital_Name); // Hospital Name : Apollo Pvt Ltd
+console.log("Patient Name : "+Patient_Name);  // Patient Name : Ganesh
+console.log("Token Number : "+Token_Number); // Token Numner : 23
+
+//5. Movie Ticket Booking
+const Theatre_Name = "Sandhya 70mm";
+let Movie_Name = prompt("Enter the name of the movie");
+let Customer_Name = prompt("Enter the name of the customer");
+let Seat_Number = "I10";
+console.log("Theatre Name : "+Theatre_Name); // Theatre Name : Sandhya 70mm
+console.log("Movie Name : "+Movie_Name);  // Movie Name : Okkadu
+console.log("Customer Name : "+Customer_Name); // Customer Name : Sivaganesh
+console.log("Seat Number : "+Seat_Number); // Seat Number : I10
+
+//6. Mobile shop
+const Shop_Name = "Poorvika Mobiles";
+let Customer_Name = prompt("Enter the customer name");
+let Mobile_Model = prompt("Name of the mobile model");
+let Price = 12500;
+console.log("Shop Name : "+Shop_Name);  // Shop Name : Poorvika Mobiles
+console.log("Name of the Customer :"+Customer_Name); // Name of the Customer : Siva
+console.log("Mobile Model : "+Mobile_Model); // Mobile Model : Iphone 15 Pro Max
+console.log("Price : "+Price); // Price : 12500
+
+//7. Gym Membership
+const Gym_Name = "Cult Fit";
+let Member_Name = prompt("Enter the name of the member : ");
+let Membership_Fee = 3000;
+console.log("Name of the Gym : "+Gym_Name); // Name of the Gym : Cult Fit
+console.log("Member Name : "+Member_Name); // Member Name : Sivaganesh
+console.log("Membership Fee : "+Membership_Fee); // Membership Fee : 3000
+
+//8. ATM Welcome Screen
+const Bank_Name = "State Bank of India";
+let User_Name = prompt("Enter the user name");
+console.log("Welcome "+User_Name); // Welcome Sivaganesh
+console.log("Bank Name : "+Bank_Name); // Bank Name : State bank of India
+
+//9.Bus Ticket
+const Travels_Name = "RS Travels";
+let Passenger_Name = prompt("Enter the name of the passenger");
+let Seat_Number = "LB11";
+console.log("Travels Name : "+Travels_Name); // Travels Name : RS Travels
+console.log("Name of the passenger : "+Passenger_Name); // Name of the passenger : Sivaganesh
+console.log("Seat No : "+Seat_Number); // Seat No : LB11
+
+//10.Online Shopping 
+const Website_Name = "Myntra";
+let Product_Name = prompt("Name of the product");
+let Customer_Name = prompt("Name of the customer");
+let Price = 950;
+console.log("Website Name : "+Website_Name); // Website Name : Myntra
+console.log("Name of the product : "+Product_Name); // Name of the product : shoes
+console.log("Name of the customer : "+Customer_Name); // Name of the customer : Sivaganesh
+console.log("Price : "+Price); // Price : 950
+
+//Task - 11
+console.log("Registration Successful");  // Registration Successful
+
+//Task - 12
+console.error("Payment Failed"); //Payment Failed
+
+//Task - 13
+console.warn("Battery Low") //Battery Low
+
+//Task - 14 
+console.log("Loading...");
+console.log("welcome");
+console.clear() // Console was cleared
+
+//Task - 15 
+alert("Welcome to Amazon"); // Welcome to Amazon
+
+//Task - 16
+let logout = confirm("Do you want to logout?");
+console.log(logout); //true
+
+//Task - 17
+let City = prompt("Enter your city name");
+console.log("Your City : "+City) //Your City : Vizag
+
+//Task - 18
+var product = "Laptop";
+product = "Mobile";
+var product = "Tablet";
+console.log(product); // Tablet
+
+//Task - 19
+let Salary = 25000;
+Salary = 30000;
+console.log(Salary); //30000
+
+//Task - 20
+const Company = "Google";
+console.log(Company); //Google
+
+
+
+
+
